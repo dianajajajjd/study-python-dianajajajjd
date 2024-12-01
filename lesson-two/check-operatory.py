0a2ac557a9a0709423562722b5ce89f321eb9468
dianajajajjd <dianaartemovnapetrovskaya@gmail.com>
2024-12-01 13:40:30 +0200

    .\lesson-two\

---
+++ lesson-two/check-operatory.py
@@ -0,0 +1,14 @@
+lst = [1,2,3,4,5,6,7,8,9]
+dct = {'name': "HIHI", 'age': 10}
+name = "HIHI"
+tpl = "H", "O", "C"
+
+result = dct ['age'] in lst
+print(result)
+
+print(dct['age'] == dct['name'])
+
+print(dct['name'] == name and dct['age'] == 10)
+
+
+
---
+++ lesson-two/zminni+operatory.py
@@ -0,0 +1,34 @@
+#String -
+string = "HI, how have you been?"
+print(type(string))
+
+#Integer -
+int = 12345
+print(type(int))
+
+#Float -
+num_2 = 12.345
+print(type(num_2))
+
+#Bool -
+check = True
+print(type(check))
+
+#List -
+lst = ["HI", "how", "have", "you", "been"]
+print(type(lst))
+
+#Dictionary -
+dct = {'Name': "LOLO", 'Age': 99}
+print(type(dct))
+
+#Tuple -
+num_3 = 12, 34, 5
+print(type(num_3))
+
+set_ex = {12, 34, 56, 12}
+print(set_ex)
+print(type(set_ex))
+
+print(type(None))
+
